55db89435b6223b4ee674ea068b1875712a99ad1
unknown <ribanez2@uc.cl>
2022-06-03 16:10:55 -0400

    cambios archivo

--- Actividades/AC00/oop 1_2.py
+++ Actividades/AC00/oop 1_2.py
@@ -2,6 +2,11 @@
 Probando propiedades de los objetos
 '''
 
+'''
+property() permite crear una propiedad, reciviendo de argumentos
+las funciones, get, set y delete del atributo
+prpoerty(<setter_function>,<getter_funtion>,<deleter_function>)
+'''
 
 class Email:
 
@@ -26,12 +31,18 @@ class Email:
     email = property(_get_email, _set_email, _del_email,
                      'This property contains the email.')
 
+'''
+La primera diferencia que vemos entre los códigos anteriores
+es el uso de __ antes de mi_atributo. Cuando nombramos una variable de esta
+manera, es una forma de decirle a Python que queremos
+que se “oculte” y que no pueda ser accedida como el resto de atributos.
+'''
 
 # Ejemplo de funcionamiento
 
 m1 = Email('kp1@othermail.com')
 print(m1.email)
-m1.email = "kp2@com"
+m1.email = "kp2@com" #se puede usar tanto con _ como sin el
 print(m1.email)
 
     
